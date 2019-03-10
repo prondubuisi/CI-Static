6d3a657e40880ee5aa3015e79d0c399fa4c2238c
Onyemenam Ndubuisi <onyemenamndu@gmail.com>
2019-03-10 11:57:34 +0100

    setup initial test

---
+++ tests/TestNumber.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase as TestCase;
+class TestNumber extends TestCase {
+
+   function testNumberequals() {
+        
+    $this->assertEquals(
+        14,
+        14,
+        "should be fourteen"
+    );
+    }
+}
